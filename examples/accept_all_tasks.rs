2311cae1b27007be0f6f2055f665227cdaf7f4a7
stneng <git@stneng.com>
2022-07-06 14:30:09 +0000

    examples

---
+++ examples/accept_all_tasks.rs
@@ -0,0 +1,30 @@
+use colink_policy_module::colink_policy_module_proto::*;
+use colink_sdk_a::CoLink;
+use prost::Message;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    let addr = &args[0];
+    let jwt = &args[1];
+
+    let cl = CoLink::new(addr, jwt);
+    let mut settings: Settings = match cl.read_entry("_policy_module:settings").await {
+        Ok(res) => prost::Message::decode(&*res)?,
+        Err(_) => Default::default(),
+    };
+    let rule_id = uuid::Uuid::new_v4().to_string();
+    let rule = Rule {
+        rule_id: rule_id.clone(),
+        task_filter: Some(TaskFilter::default()),
+        action: "approve".to_string(),
+        priority: 1,
+    };
+    settings.rules.push(rule);
+    let mut payload = vec![];
+    settings.encode(&mut payload).unwrap();
+    cl.update_entry("_policy_module:settings", &payload).await?;
+
+    Ok(())
+}
---
+++ examples/start_policy_module.rs
@@ -0,0 +1,29 @@
+use colink_sdk_a::{CoLink, Participant};
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    let addr = &args[0];
+    let jwt = &args[1];
+
+    let cl = CoLink::new(addr, jwt);
+    let participants = vec![Participant {
+        user_id: cl.get_user_id()?,
+        ptype: "local".to_string(),
+    }];
+    let task_id = cl
+        .run_task(
+            "policy_module.start",
+            Default::default(),
+            &participants,
+            false,
+        )
+        .await?;
+    println!(
+        "Local task {} has been created, it will remain in started status until the protocol starts.",
+        task_id
+    );
+
+    Ok(())
+}
